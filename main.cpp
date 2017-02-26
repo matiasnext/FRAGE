281f13b3947fd1ef346c74c965b74d540d573e2c
matiasnext <matiasnext@hotmail.com>
2017-02-26 19:54:12 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void mostrar();
+
+int main()
+{
+  int x,y;
+	cout << "Ingresaar coordenada x" << endl;
+	cin >> x;
+	cout << "Ingresaar coordenada y" << endl;
+	cin >> y;
+	mostrar();
+return 0;
+}
+
+void mostrar ()
+{
+	cout << "El par ordenado es: ";
+	cout << "(" << x << "," << y << ")" << endl;
+}
