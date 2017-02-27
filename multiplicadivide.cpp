cae6016a17d1186d861029adfd23ddfa4567f3ab
matiasnext <matiasnext@hotmail.com>
2017-02-27 12:27:56 -0300

    Update multiplicadivide.cpp

--- multiplicadivide.cpp
+++ multiplicadivide.cpp
@@ -11,14 +11,14 @@ int main()
 	cin >> num1;
 	cout << "ingresar otro numero:" << endl;
 	cin >> num2;
-    multiplica_divide(num1,num2);
+	multiplica_divide(num1,num2);
 return 0;
 }
 
 void multiplica_divide(int a, int b)
 {
 	if(a%b==0)
-        cout << "La multiplicacion es: " << a*b;
+		cout << "La multiplicacion es: " << a*b;
 	else
 		cout << "La division es: " << (float)a/b;
 return;
