cef79f35b1c045a3cc6912940d048f6a01d9cd2a
matiasnext <matiasnext@hotmail.com>
2017-02-27 12:42:12 -0300

    Update matriz.cpp

--- matriz.cpp
+++ matriz.cpp
@@ -6,21 +6,18 @@ using namespace std;
 int main()
 {
     int filas, columnas;
-    cout<<"Ingrese numero de filas  :";
-    cin>>filas;
-    cout<<"Ingrese numero de columnas :";
-    cin>>columnas;
-
-     int matriz[filas][columnas];
-
-
+    cout << "Ingrese numero de filas: ";
+    cin >> filas;
+    cout << "Ingrese numero de columnas: ";
+    cin >> columnas;
+    int matriz[filas][columnas];
     for(int i =0; i< filas ; i++)
     {
-          for(int j =0; j< columnas ;j++)
-          {
-            matriz[i][j]=rand()%10;
-            cout<<matriz[i][j]<<"  ";
-          }
-  cout<<endl;
+        for(int j =0; j< columnas ;j++)
+        {
+             matriz[i][j]=rand()%10; //rand para rellenar la matriz con numeros aleatorios
+             cout<<matriz[i][j]<<" ";
+        }
+    cout<<endl;
     }
 }
