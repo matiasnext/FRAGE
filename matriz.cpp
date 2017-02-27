981f1f1a021d25ef97bbcc9c1ddd51f710e3fe60
matiasnext <matiasnext@hotmail.com>
2017-02-27 12:38:54 -0300

    Create matriz.cpp

---
+++ matriz.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    int filas, columnas;
+    cout<<"Ingrese numero de filas  :";
+    cin>>filas;
+    cout<<"Ingrese numero de columnas :";
+    cin>>columnas;
+
+     int matriz[filas][columnas];
+
+
+    for(int i =0; i< filas ; i++)
+    {
+          for(int j =0; j< columnas ;j++)
+          {
+            matriz[i][j]=rand()%10;
+            cout<<matriz[i][j]<<"  ";
+          }
+  cout<<endl;
+    }
+}
